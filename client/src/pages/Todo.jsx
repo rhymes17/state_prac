368d6ba92a29ee57b6a7407f6108a9d41544224a
Rahul Dev Choudhari <irhymecodes@gmail.com>
2024-04-12 02:03:41 +0530

    Get todos

--- client/src/pages/Todo.jsx
+++ client/src/pages/Todo.jsx
@@ -1,20 +1,42 @@
 import React from "react";
 import TodoItem from "../components/TodoItem";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const Todo = () => {
-  const todoItem = {
-    id: 1,
-    todo: "Do something nice for someone I care about",
-    completed: true,
-    userId: 26,
-    priority: "High",
+  const BASE_URL = "http://localhost:8000/api/todo";
+
+  const getTodos = async () => {
+    try {
+      const res = await axios.get(`${BASE_URL}`);
+      return res.data.data;
+    } catch (error) {
+      return error;
+    }
   };
+
+  const {
+    data: todoItems,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["todos"],
+    queryFn: getTodos,
+  });
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>Error....</h1>;
+  }
 
   return (
     <div className="my-5 h-[55vh] overflow-y-scroll no-scrollbar flex flex-col gap-8 w-[90%] mx-auto">
-      <TodoItem todoItem={todoItem} />
-      <TodoItem todoItem={todoItem} />
-      <TodoItem todoItem={todoItem} />
+      {todoItems?.map((todoItem) => (
+        <TodoItem key={todoItem._id} todoItem={todoItem} />
+      ))}
     </div>
   );
 };
